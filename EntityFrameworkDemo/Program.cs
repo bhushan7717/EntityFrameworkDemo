df1eec0c3ccfabb1ae4c8098e5e998f16144b419
Admin <bhushan.wadile@yahoo.com>
2021-11-25 22:15:34 +0530

    Add project files.

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using EntityFrameworkDemo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PerformDatabaseOperation();
+			Console.WriteLine("Quote of the day");
+			Console.WriteLine(" Don't worry about the world coming to an end today... ");
+			Console.WriteLine(" It's already tomorrow in Australia.");
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+		}
+
+		public static void PerformDatabaseOperation()
+		{
+			using(var db = new BloggingContext())
+			{
+				db.Blogs.Add(new Blog()
+				{
+					BlogId = (db.Blogs.Count() + 1),
+					Name = "Test Blog " + (db.Blogs.Count() + 1)
+				});
+
+				Console.WriteLine("Calling save changes.");
+				db.SaveChanges();
+				Console.WriteLine("Save changes completed.");
+
+				Console.WriteLine("Executing query.");
+
+				var blogs = (from b in db.Blogs
+							 orderby b.BlogId
+							 select b).ToList();
+
+				Console.WriteLine("Query completed with following results.");
+
+				foreach (var item in blogs)
+				{
+					Console.WriteLine(" " + item.Name);
+				}
+			}
+		}
+	}
+}
---
+++ EntityFrameworkDemo/Readme - bhushan.txt
@@ -0,0 +1,4 @@
+This is default entity framework implementation
+-- check for the new connection string option
+-- use microsoft docs for the tutorial
+https://docs.microsoft.com/en-us/ef/ef6/fundamentals/working-with-dbcontext
