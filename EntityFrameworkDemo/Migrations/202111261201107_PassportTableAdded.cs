40bb8ea44cc20d2c578045e1f3965c2b9494e659
Admin <bhushan.wadile@yahoo.com>
2021-11-26 21:21:18 +0530

    Revert "Revert "Entity framework implementation complete.""
    This reverts commit e31306d1a188ae713febee7fc93ec70b086412db.

---
+++ EntityFrameworkDemo/Migrations/202111261201107_PassportTableAdded.cs
@@ -0,0 +1,16 @@
+﻿namespace EntityFrameworkDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PassportTableAdded : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- EntityFrameworkDemo/Program.cs
+++ EntityFrameworkDemo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using EntityFrameworkDemo.Data;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,7 @@ namespace EntityFrameworkDemo
 {
 	class Program
 	{
+		
 		static void Main(string[] args)
 		{
 			PerformDatabaseOperation();
@@ -31,21 +34,58 @@ namespace EntityFrameworkDemo
 					Name = "Test Blog " + (db.Blogs.Count() + 1)
 				});
 
+
+				db.Posts.Add(new Post()
+				{
+					PostId = db.Posts.Count() + 1,
+					BlogId =  1,
+					Title = "Title " + (db.Posts.Count() + 1),
+					Content = "Content " + (db.Posts.Count() + 1)
+				});
+
+				//db.departments.Add(new Department()
+				//{
+  
+				//})
+
+				//db.Posts.Add(new Post()
+				//{
+				//	PostId = db.Posts.Count() + 1,
+				//	BlogId = 2,
+				//	Title = "Title " + (db.Posts.Count() + 1),
+				//	Content = "Content " + (db.Posts.Count() + 1)
+				//});
+
 				Console.WriteLine("Calling save changes.");
 				db.SaveChanges();
 				Console.WriteLine("Save changes completed.");
 
-				Console.WriteLine("Executing query.");
+				Console.WriteLine("Executing Blog query.");
 
 				var blogs = (from b in db.Blogs
 							 orderby b.BlogId
 							 select b).ToList();
+
+				var blogs1 = db.Blogs.Include("Posts").ToList();
 
 				Console.WriteLine("Query completed with following results.");
 
 				foreach (var item in blogs)
 				{
 					Console.WriteLine(" " + item.Name);
+				}
+
+				Console.WriteLine("Executing Post query.");
+
+				var posts = (from b in db.Posts
+							 orderby b.PostId
+							 select b).ToList();
+
+				Console.WriteLine("Query completed with following results.");
+
+				foreach (var item in posts)
+				{
+					Console.WriteLine(" " + item.Title);
 				}
 			}
 		}
