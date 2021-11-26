6bb1596935dbc58f9186eb968002dfebcafb3b56
Admin <bhushan.wadile@yahoo.com>
2021-11-26 21:05:37 +0530

    Entity framework implementation complete.

---
+++ EntityFrameworkDemo/Migrations/202111261219232_DeptWithEnum.cs
@@ -0,0 +1,27 @@
+﻿namespace EntityFrameworkDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeptWithEnum : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        DeptId = c.Int(nullable: false, identity: true),
+                        departmentNames = c.Int(nullable: false),
+                        budget = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.DeptId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Departments");
+        }
+    }
+}
